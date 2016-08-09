4fee2431c1dd72612c9c38c2f8a09e465370ecf8
trevor millner <trevormillner@yahoo.com>
2016-08-09 18:41:08 +0800

    task - setup google drive auth

--- googley/googley.js
+++ googley/googley.js
@@ -1,20 +1,27 @@
-if (!process.env.token) {
-    console.log('Error: Specify token in environment');
+if (!process.env.bot_token) {
+    console.log('Error: Specify bot token in environment');
+    process.exit(1);
+}
+
+if (!process.env.google_token) {
+    console.log('Error: Specify google api token in environment');
     process.exit(1);
 }
 
 var Botkit = require('botkit');
 var os = require('os');
+var https = require('https');
 
 var controller = Botkit.slackbot({
     debug: true
 });
 
 var bot = controller.spawn({
-    token: process.env.token
+    token: process.env.bot_token
 }).startRTM();
 
-/*
+var google_token = process.env.google_token;
+
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
@@ -27,6 +34,16 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
         }
     });
 
+var options = {
+  host: 'https://www.googleapis.com/drive/v2',
+  path: '/files'
+};
+
+https.get('https://www.googleapis.com/drive/v2/files?access_token=' + google_token, function(res) {
+  console.log("Google API response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Google API  error: " + e.message);
+});
 
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
@@ -36,7 +53,6 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
         }
     });
 });
-*/
 
 controller.hears(['files updated today'], 'direct_message,direct_mention,mention', function(bot, message) {
     
